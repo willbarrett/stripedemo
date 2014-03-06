5dbede663707a48d69bc2ce0b0fc3c7048152010
Will Barrett <will@tallgreentree.com>
2014-03-06 07:30:42 -0600

    New Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stripedemo::Application.config.secret_key_base = '61a7f2be99159800973633ef626686a1acdce2fd2afdcbdea99976b2c2b7c9d1908210fa7f99d4e8fc98b31bbcfbfe0bf0ea97d2fa0b4e82b4fd85b4ea4b6e29'
